353970b79bec22b18638fafc511355bd6623a42b
Marc Kevin Zenzen <marc-kevin.zenzen@computacenter.com>
2023-11-30 12:48:00 +0100

    now produces either a shopping basket or an order

--- eCommerceDataGenerator/Program.cs
+++ eCommerceDataGenerator/Program.cs
@@ -20,15 +20,16 @@ string KAFKA_BROKER = configuration.GetValue<string>("Kafka:Broker")!;
 string KAFKA_TOPIC = configuration.GetValue<string>("Kafka:Topic")!;
 bool KAFKA_DELETE_TOPIC_FIRST = configuration.GetValue<bool>("Kafka:DeleteTopicFirst")!;
 int AMOUNT_OF_USERS_TO_GENERATE = configuration.GetValue<int>("Kafka:AmountOfUsersToGenerate")!;
+bool createPaymentOrShoppingBasket = configuration.GetValue<bool>("CreatePaymentOrShoppingBasket")!;
 
 
 // Generate mock order
 var mockOrder = new Faker<Order>()
-    .RuleFor(o => o.OrderId, new Guid())
+    .RuleFor(o => o.OrderId, f => Guid.NewGuid())
     .RuleFor(o => o.CreateDate, (f, o) => f.Date.BetweenDateOnly(
                     new DateOnly(2000, 1, 1),
                     new DateOnly(2022, 12, 1)))
-    .RuleFor(o => o.Status, (f, u) => f.IndexFaker == 0 ? true : false)
+    .RuleFor(o => o.Status, (f, u) => false) //for random true or false -> f.IndexFaker == 0 ? true : false)
     .RuleFor(u => u.PaymentDate, (f, o) => f.Date.BetweenDateOnly(
                     new DateOnly(2003, 1, 1),
                     new DateOnly(2023, 1, 3)));
@@ -63,25 +64,34 @@ ProducerConfig configProducer = new ProducerConfig
 };
 
 // Produce user
-using var producer = new ProducerBuilder<Null, string>(configProducer).Build();
-foreach (var user in mockGeneratedUsers)
+// to create an shoppingbasket => appsatings -> createPaymentOrShoppingBasket = true
+if (createPaymentOrShoppingBasket)
 {
-    var result = await producer.ProduceAsync(KAFKA_TOPIC, new Message<Null, string>
+    using var producer = new ProducerBuilder<Null, string>(configProducer).Build();
+    foreach (var user in mockGeneratedUsers)
     {
-        Value = JsonSerializer.Serialize<User>(user)
-    });
-    Console.WriteLine(JsonSerializer.Serialize<User>(user));
+        var result = await producer.ProduceAsync(KAFKA_TOPIC, new Message<Null, string>
+        {
+            Value = JsonSerializer.Serialize<User>(user)
+        });
+        Console.WriteLine(JsonSerializer.Serialize<User>(user));
 
+    }
 }
 
 // Produce Order
-using var producer1 = new ProducerBuilder<Null, string>(configProducer).Build();
-foreach (var order in mockGeneratedOrders)
+// to create an order => appsatings -> createPaymentOrShoppingBasket = false
+if (!createPaymentOrShoppingBasket)
 {
-    var result = await producer1.ProduceAsync(KAFKA_TOPIC, new Message<Null, string>
+    using var producer1 = new ProducerBuilder<Null, string>(configProducer).Build();
+    foreach (var order in mockGeneratedOrders)
     {
-        Value = JsonSerializer.Serialize<Order>(order)
-    });
-    Console.WriteLine(JsonSerializer.Serialize<Order>(order));
+        var result = await producer1.ProduceAsync(KAFKA_TOPIC, new Message<Null, string>
+        {
+            Value = JsonSerializer.Serialize<Order>(order)
+        });
+        Console.WriteLine(JsonSerializer.Serialize<Order>(order));
 
+    }
 }
+
